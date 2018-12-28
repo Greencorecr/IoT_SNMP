f1e4f5adcd057cb353cc40805630a3ace134e8e7
Alvaro Figueroa <alvaro@greencore.co.cr>
2018-12-28 14:01:54 -0600

    Travis: Agregando tinydb a test.py de SNMP

--- Conectores/SNMP/db2snmp/db2snmp.py
+++ Conectores/SNMP/db2snmp/db2snmp.py
@@ -7,8 +7,15 @@ Sensores = Query()
 
 search=db.get(Sensores.dev_id == 'ttgo-mapper')
 
+def TTN2time(search):
+    """
+    Convierte el formato de hora de TTN, a algo usable con datetime
+    """
+    timeTTN = datetime.strptime((search.get("time")[0:10] + " " + search.get("time")[11:19]), "%Y-%m-%d %H:%M:%S")
+    return timeTTN
+
 # Medimos el tiempo según TTN, el tiempo de hace 5 minutos. Si no hemos recibido datos, el sensor responde "0"
-timeTTN = datetime.strptime((search.get("time")[0:10] + " " + search.get("time")[11:19]), "%Y-%m-%d %H:%M:%S")
+timeTTN=TTN2time(search)
 timeFail=datetime.utcnow()
 # Debug
 #print (timeFail - timedelta(minutes=5), timeTTN)
--- Conectores/SNMP/test/test.py
+++ Conectores/SNMP/test/test.py
@@ -1,23 +1,28 @@
 import unittest
 from datetime import datetime, timedelta
+from tinydb import TinyDB, Query
+from db2snmp.db2snmp import TTN2time
+
+
 
 class PruebaConector(unittest.TestCase):
     """
-      Comprueba las base de datos y la manipulacion de fechas
+    Comprueba las base de datos y la manipulacion de fechas
     """
  
-    def test_resta_tiempo(self):
+    def testRestaTiempo(self):
         """
         Revisa la formula para verificar si han pasado 5 minutos desde 
         la última actualización
         """
-        timeMQTT="2018-12-27T23:30:00.000000000Z"
-        timeTTN=datetime.strptime(timeMQTT[0:10] + " " + timeMQTT[11:19], "%Y-%m-%d %H:%M:%S")
-
+        db = TinyDB('../MQTT/test/sensores.json')
+        Sensores = Query()
+        search=db.get(Sensores.dev_id == 'sensor-demo')
+        timeTTN = TTN2time(search)
         timeFail=datetime.strptime("2018-12-27 23:35:00",  "%Y-%m-%d %H:%M:%S")
         result =  (timeFail - timedelta(minutes=5) == timeTTN)
-        print (result)
-        self.assertEqual(result, True)
+        db.close()
+        self.assertTrue(result)
 
 
 if __name__ == '__main__':
