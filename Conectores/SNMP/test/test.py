c802d81dedb4ab007ec2ab0c8668d61e9a91a3e3
Alvaro Figueroa <alvaro@greencore.co.cr>
2018-12-27 21:40:48 -0600

    Travis: Renombrando a test/test.py, cambios menores

--- Conectores/SNMP/test/test.py
+++ Conectores/SNMP/test/test.py
@@ -11,8 +11,8 @@ class PruebaTiempo(unittest.TestCase):
         la última actualización
         """
         timeMQTT="2018-12-27T23:35:00.000000000Z"
-        timeTTN=timeMQTT[0:10] + " " + timeMQTT("time")[11:19])
+        timeTTN=timeMQTT[0:10] + " " + timeMQTT("time")[11:19]
 
         timeFail="2018-12-27 23:30:00.000000"
-        result = timeFail - timedelta(minutes=5) = timeTTN
+        result =  (timeFail - timedelta(minutes=5) == timeTTN)
         self.assertEqual(result, true)
