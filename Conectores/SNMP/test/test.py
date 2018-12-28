dbe72d34b87f29b8e3ae908b2b9979cc26bcf7ca
Alvaro Figueroa <alvaro@greencore.co.cr>
2018-12-27 21:59:26 -0600

    Travis: Cambios menores en test.py

--- Conectores/SNMP/test/test.py
+++ Conectores/SNMP/test/test.py
@@ -1,21 +1,23 @@
 import unittest
- 
-class PruebaTiempo(unittest.TestCase):
+from datetime import datetime, timedelta
+
+class PruebaConector(unittest.TestCase):
     """
-    
+      Comprueba las base de datos y la manipulacion de fechas
     """
  
-    def prueba_resta_tiempo(self):
+    def test_resta_tiempo(self):
         """
         Revisa la formula para verificar si han pasado 5 minutos desde 
         la última actualización
         """
-        timeMQTT="2018-12-27T23:35:00.000000000Z"
-        timeTTN=timeMQTT[0:10] + " " + timeMQTT("time")[11:19]
+        timeMQTT="2018-12-27T23:30:00.000000000Z"
+        timeTTN=datetime.strptime(timeMQTT[0:10] + " " + timeMQTT[11:19], "%Y-%m-%d %H:%M:%S")
 
-        timeFail="2018-12-27 23:30:00.000000"
+        timeFail=datetime.strptime("2018-12-27 23:35:00",  "%Y-%m-%d %H:%M:%S")
         result =  (timeFail - timedelta(minutes=5) == timeTTN)
-        self.assertEqual(result, true)
+        print (result)
+        self.assertEqual(result, True)
 
 
 if __name__ == '__main__':
