f7d0173942f352217dc87a80e1ca00eaadb20155
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-02-04 10:49:03 -0600

    Version con problemas para imprimir datos de sensor en pantalla de serial2ttn.ino

---
+++ Conectores/Arduino/Amperaje/Arduino-Nano/serial2ttn/serial2ttn.ino
@@ -0,0 +1,325 @@
+/*
+ * Sensor de consumo eléctrico
+ * Para ESP32 TTGOv2
+ * por Greencore Solutions
+ * Usa serial2 y empaqueta hacia TTN
+ * 
+ * Debe definir NWKSKEY, APPSKEY, y DEVADDR
+ */
+
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+#include <Wire.h>
+#include<U8g2lib.h>
+#include<Arduino.h>
+
+
+// Imagen de GreenCore 50x50 px
+#define greenfoot_width 50
+#define greenfoot_height 50
+static const unsigned char greenfoot_bits[] PROGMEM = {
+   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfc, 0xff, 0xff, 0xff, 0xff,
+   0xff, 0x00, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01, 0xff, 0xff, 0xff,
+   0xff, 0xff, 0xff, 0x03, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x03, 0xff,
+   0xff, 0xff, 0xff, 0xff, 0xff, 0x03, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+   0x03, 0xff, 0xff, 0xff, 0x7f, 0xf0, 0xff, 0x03, 0xff, 0xc0, 0xff, 0x3f,
+   0xe0, 0xff, 0x03, 0x7f, 0xc0, 0xff, 0x1f, 0xc0, 0xff, 0x03, 0x3f, 0x80,
+   0xff, 0x0f, 0xc0, 0xff, 0x03, 0x3f, 0x80, 0xff, 0x0f, 0xc0, 0xff, 0x03,
+   0x3f, 0x80, 0xff, 0x0f, 0xc0, 0xff, 0x03, 0x3f, 0x00, 0xff, 0x0f, 0xc0,
+   0xff, 0x03, 0x3f, 0x80, 0xff, 0x0f, 0xc0, 0xff, 0x03, 0x7f, 0x80, 0xff,
+   0x07, 0xc0, 0xff, 0x03, 0x7f, 0xc0, 0xff, 0x07, 0xe0, 0xff, 0x03, 0x7f,
+   0xe0, 0xff, 0x03, 0xf8, 0xff, 0x03, 0xff, 0xe1, 0xff, 0x81, 0xff, 0xff,
+   0x03, 0xff, 0xf1, 0xff, 0xc0, 0xff, 0xff, 0x03, 0xff, 0xf3, 0x7f, 0xe0,
+   0xff, 0xff, 0x03, 0xff, 0xe3, 0x7f, 0xf0, 0xff, 0xff, 0x03, 0xff, 0xe3,
+   0x3f, 0xf8, 0xff, 0xff, 0x03, 0xff, 0xe7, 0x1f, 0xfc, 0xff, 0xff, 0x03,
+   0xff, 0xc7, 0x0f, 0xfe, 0xff, 0xff, 0x03, 0xff, 0xc7, 0x07, 0xff, 0xff,
+   0xff, 0x03, 0xff, 0x87, 0x83, 0xff, 0xff, 0xff, 0x03, 0xff, 0x07, 0xc0,
+   0xff, 0xff, 0xff, 0x03, 0xff, 0x07, 0xc0, 0xff, 0xff, 0xff, 0x03, 0xff,
+   0x07, 0xe0, 0xff, 0x1f, 0xfc, 0x03, 0xff, 0x07, 0xe0, 0xff, 0x07, 0xf8,
+   0x03, 0xff, 0x03, 0xf0, 0xff, 0x07, 0xf0, 0x03, 0xff, 0x03, 0xf0, 0xff,
+   0x03, 0xf0, 0x03, 0xff, 0x01, 0xf0, 0xff, 0x01, 0xf0, 0x03, 0xff, 0x00,
+   0xe0, 0xff, 0x00, 0xf0, 0x03, 0xff, 0x00, 0x80, 0x1f, 0x00, 0xf0, 0x03,
+   0x7f, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x03, 0x3f, 0x00, 0x00, 0x00, 0x00,
+   0xf8, 0x03, 0x1f, 0x00, 0x00, 0xe0, 0x0f, 0xfc, 0x03, 0x0f, 0x00, 0x00,
+   0xfe, 0x1f, 0xfe, 0x03, 0x07, 0x00, 0xe0, 0xff, 0xff, 0xff, 0x03, 0x03,
+   0x00, 0xf8, 0xff, 0xff, 0xff, 0x03, 0x01, 0x00, 0xfe, 0xff, 0xff, 0xff,
+   0x03, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0x03, 0x00, 0xc0, 0xff, 0xff,
+   0xff, 0xff, 0x03, 0x00, 0xe0, 0xff, 0xff, 0xff, 0xff, 0x03, 0x00, 0xf0,
+   0xff, 0xff, 0xff, 0xff, 0x03, 0x00, 0xf8, 0xff, 0xff, 0xff, 0xff, 0x01,
+   0x00, 0xf8, 0xff, 0xff, 0xff, 0xff, 0x01, 0x00, 0xfc, 0xff, 0xff, 0xff,
+   0xff, 0x00 };
+
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the prototype TTN
+// network initially.
+static const PROGMEM u1_t NWKSKEY[16] = {  }; // Network Session Key, hex, MSB
+
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the prototype TTN
+// network initially.
+static const u1_t PROGMEM APPSKEY[16] = {  }; // Application Sessoin Key, hex, MSB
+
+// LoRaWAN end-device address (DevAddr)
+// See http://thethingsnetwork.org/wiki/AddressSpace
+static const u4_t DEVADDR = 0xfede2 ; // Device Address, 0xDecimal, MSB
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16); 
+
+unsigned long previousMillis = 0;
+const long interval = 300000;
+unsigned int paqCont = 0;
+uint8_t mydata[] = "00000";
+
+// Para lectura de Serial2
+String inputString = "";      // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 18,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 14,
+    .dio = {26, 33, 32}  // Pins for the Heltec ESP32 Lora board/ TTGO Lora32 with 3D metal antenna
+};
+
+
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if(LMIC.dataLen) {
+                // data received in rx slot after tx
+                Serial.print(F("Data Received: "));
+                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+                Serial.println();
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+
+void do_send(osjob_t* j){
+    unsigned int sensorCount = 0;
+    String sensorData[4] = "0000";
+    // LOGO
+    u8g2.clearBuffer();
+    u8g2.clearDisplay();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawXBMP(39,0,50,50,greenfoot_bits);
+    u8g2.drawStr(5,64,"GreenCore Solutions");
+    u8g2.sendBuffer();
+    delay(5000); // muestra el logo por 5 segundos  
+
+    // TODO: Mover a funcion
+    while (Serial2.available()) {
+      // get the new byte:
+      char inChar = (char)Serial2.read();
+      // add it to the inputString:
+      if (inChar == ':') {
+        sensorData[sensorCount] = inputString;
+        inputString="";
+        sensorCount++;        
+      } else if (inChar == '\n') {
+        sensorData[3]= inputString;
+        Serial.print("sensorData[0]: ");
+        Serial.println(sensorData[0]);
+        Serial.print("sensorData[1]: ");
+        Serial.println(sensorData[1]);
+        Serial.print("sensorData[2]: ");
+        Serial.println(sensorData[2]);
+        Serial.print("sensorData[3]: ");
+        Serial.println(sensorData[3]); 
+        inputString = "";
+        stringComplete = true;
+        sensorCount = 0;
+      } else {
+        inputString += inChar;
+      }
+    } 
+
+    mydata[1] = sensorData[0].toInt();
+    mydata[2] = sensorData[1].toInt();
+    mydata[3] = sensorData[2].toInt();
+    mydata[4] = sensorData[3].toInt();
+
+    unsigned long currentMillis = millis();
+    
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+           // Prepare upstream data transmission at the next possible time.
+           LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+        }
+    Serial.println(F("Packet queued"));
+    Serial.println(LMIC.freq);
+
+ // INFORMACIÓN GENERAL
+    char frecString[10]; 
+    dtostrf(LMIC.freq/1000000.0,3,2,frecString);
+    char paqString02[10]; 
+    dtostrf(paqCont,2,0,paqString02);
+    u8g2.clearBuffer();
+    u8g2.clearDisplay();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr(0, 10, "Consumo Eléctrico");
+    u8g2.drawStr(0, 20, "Frecuencia: ");
+    u8g2.drawStr(68,20,frecString);
+    u8g2.drawStr(100,20," MHz");
+    u8g2.drawStr(0,30,"Paquete: ");
+    u8g2.drawStr(50,30,paqString02);
+// TODO
+
+/*    
+    char sensorData0[3];
+    dtostrf(String(sensorData[0]),3,0,sensorData0);
+    char sensorData1[10];
+    dtostrf(sensorData[1],3,0,sensorData1);
+    char sensorData2[10];
+    dtostrf(sensorData[2],3,0,sensorData2);
+    char sensorData3[10];
+    dtostrf(sensorData[3],3,0,sensorData3);
+    */
+    u8g2.drawStr(0,40,"s1: ");
+    u8g2.drawStr(60,40,"s2: ");
+    u8g2.drawStr(0,50,"s3: ");
+    u8g2.drawStr(60,50,"s4: ");
+
+//char sensorData0[3];
+//sensorData0 = String(sensorData[0]);
+//    u8g2.drawStr(10,40,sensorData0);
+//    u8g2.drawStr(10,40,sensorData[0]);
+//    u8g2.drawStr(60,40,sensorData[1]);
+//    u8g2.drawStr(10,50,sensorData[2]);
+//    u8g2.drawStr(60,50,sensorData[3]);
+
+
+    u8g2.sendBuffer();
+    delay(3000);
+    paqCont++;
+
+}
+
+void setup() {
+    Serial2.begin(115200); // Desde Arduino-Nano
+    Serial.begin(115200);
+    Serial.println(F("Starting"));
+    u8g2.begin();
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly 
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+
+   for (int channel=0; channel<8; ++channel) {
+     LMIC_disableChannel(channel);
+   }
+   for (int channel=9; channel<72; ++channel) {
+      LMIC_disableChannel(channel);
+   }
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+    // Set data rate and transmit power (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+    // tipo de sensor
+    mydata[0] = 0x01;
+
+    // Start job
+    do_send(&sendjob);
+}
+
+void loop() {
+  os_runloop_once();
+}
