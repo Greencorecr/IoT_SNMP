2e99f91403856d37e150fcb9fcd3e1c7da9e9eb5
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-02-21 18:03:03 -0600

    Eliminando delays en OTAA-Puerta, y agregando OTAA-Caida

--- TheThingsNetwork/Conectores/Arduino/OTAA/Caida/Caida.ino
+++ TheThingsNetwork/Conectores/Arduino/OTAA/Caida/Caida.ino
@@ -1,17 +1,22 @@
 /*
- * Sensor de puerta
+ * Sensor de caída eléctrica
  * Para ESP32 TTGOv2
  * por Greencore Solutions
- * Usa pin 34 en puerta1 en modo trigger
- * 
+ * Usa pin 36 con divisor de voltaje para monitorear +5V
  * Debe definir APPEUI, DEVEUI, APPKEY
  */
+
+
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
 #include <Wire.h>
+//#include <U8x8lib.h>
 #include<U8g2lib.h>
 
+
+const int analogInPin = 36;  // Analog input pin that the potentiometer is attached to
+int sensorValue = 0;        // value read from the pot
 
 // Imagen de GreenCore 50x50 px
 #define greenfoot_width 50
@@ -48,7 +53,6 @@ static const unsigned char greenfoot_bits[] PROGMEM = {
    0x00, 0xf8, 0xff, 0xff, 0xff, 0xff, 0x01, 0x00, 0xfc, 0xff, 0xff, 0xff,
    0xff, 0x00 };
 
-
 static const u1_t PROGMEM DEVEUI[8]={  } ; // Device EUI, hex, lsb
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
@@ -61,20 +65,17 @@ void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16); 
 
-
 unsigned long previousMillis = 0;
 const long interval = 300000;
 unsigned int paqCont = 0;
-uint8_t mydata[] = "00000";
-
-TaskHandle_t tempTaskHandle = NULL;
-
-
+int estado = 0;
+uint8_t mydata[] = "000";
 static osjob_t sendjob;
+
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 30;
+const unsigned TX_INTERVAL = 60;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
@@ -84,68 +85,26 @@ const lmic_pinmap lmic_pins = {
     .dio = {26, 33, 32}  // Pins for the Heltec ESP32 Lora board/ TTGO Lora32 with 3D metal antenna
 };
 
-struct Trigger {
-    const uint8_t PIN;
-    uint32_t numberKeyPresses;
-    bool pressed;
-};
-
-
-Trigger puerta1 = {34, 0, false};
-
-void IRAM_ATTR isr() {
-    puerta1.numberKeyPresses += 1;
-    puerta1.pressed = true;
-    //Serial.println(puerta1.numberKeyPresses);
-}
-
-void logo(){
-    // LOGO
-    u8g2.clearBuffer();
-    u8g2.clearDisplay();
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawXBMP(39,0,50,50,greenfoot_bits);
-    u8g2.drawStr(5,64,"GreenCore Solutions");
-    u8g2.sendBuffer();
-}
-
-void muestraDatos(){
-    int contador = 1;
-    char actString[10]; 
-
-    // Muestra cantidad de activaciones (triggers) al activarse el sensor de puerta
-    u8g2.clearBuffer();
-    u8g2.clearDisplay();
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.drawStr(15, 10, "Activaciones ");
-    
-    // Activaciones - Triggers
-    // Se envía paquete cada 30 segundos, por lo cual durante 20 segundos se reimprime el valor de puerta1.numberKeyPresses
-    // para visualizar en pantalla cuando se abre
-    while (contador != 20){
-      Serial.println(contador);
-      dtostrf(puerta1.numberKeyPresses,9,0,actString);
-
-      // Dibuja cuadro del color del fondo para ocultar dato anterior 
-      u8g2.setDrawColor(0);
-      u8g2.drawBox(0,25,164,20);
-      u8g2.sendBuffer(); 
-
-      // Activa nuevamente color default (blanco)
-      u8g2.setDrawColor(1);
-      u8g2.drawStr(25, 40,actString);
-      u8g2.sendBuffer(); 
-      delay(1000);  
-      contador++;
-    }
-}
 
 void(* resetFunc) (void) = 0; //declare reset function @ address 0
+
+void revisarEstado(){
+    // Carga datos del sensor
+    sensorValue = analogRead(analogInPin);
+    
+    // Adaptador conectado, recibe ~0.9v, no hay caída eléctrica. sensorValue == 0 cuando esta inicializando
+    if (sensorValue == 0 || sensorValue > 800){
+      estado = 0;
+    } 
+    // Adaptador desconectado, solo recibe energía de la batería, recibe ~0.5v, sí hay caída eléctrica
+    else {
+      estado = 1;
+    }
+}
 
 void onEvent (ev_t ev) {
     Serial.print(os_getTime());
     Serial.print(": ");
-    int contador = 0;
     switch(ev) {
         case EV_SCAN_TIMEOUT:
             Serial.println(F("EV_SCAN_TIMEOUT"));
@@ -177,20 +136,17 @@ void onEvent (ev_t ev) {
             break;
         case EV_TXCOMPLETE:
             Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            Serial.println("Paquete enviado");
-            Serial.println("numberKeyPresses: ");
-            Serial.print(puerta1.numberKeyPresses);
+            Serial.println("estado: ");
+            Serial.println(estado);
+            Serial.println("sensorValue: ");
+            Serial.println(sensorValue);
             if(LMIC.dataLen) {
                 // data received in rx slot after tx
                 Serial.print(F("Data Received: "));
                 Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
-                Serial.println();
             }
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-
-            // Muestra datos de activaciones/triggers
-//            muestraDatos();
             break;
         case EV_LOST_TSYNC:
             Serial.println(F("EV_LOST_TSYNC"));
@@ -214,32 +170,35 @@ void onEvent (ev_t ev) {
     }
 }
 
-
 void do_send(osjob_t* j){
 
-    mydata[1] = ( puerta1.numberKeyPresses >> 24 ) & 0xFF;
-    mydata[2] = ( puerta1.numberKeyPresses >> 16 ) & 0xFF;
-    mydata[3] = ( puerta1.numberKeyPresses >> 8 ) & 0xFF;
-    mydata[4] = puerta1.numberKeyPresses & 0xFF;
-    
     unsigned long currentMillis = millis();
 
+    // LOGO
+    u8g2.clearBuffer();
+    u8g2.clearDisplay();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawXBMP(39,0,50,50,greenfoot_bits);
+    u8g2.drawStr(5,64,"GreenCore Solutions");
+    u8g2.sendBuffer();
+    //delay(5000); // muestra el logo por 5 segundos
+  
+    // Se llama funcion de revision antes de alistar paquete
+    revisarEstado();
+    mydata[1] = estado;
 
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
         Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
-           // Prepare upstream data transmission at the next possible time.
-           LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
-        }
-    Serial.println(F("Paquete procesado"));
+    } 
+    else {
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+    }
+    Serial.println(F("Packet queued"));
     Serial.println(LMIC.freq);
 
-    // LOGO
-    logo();
-    //delay(2000); // muestra el logo por 2 segundos  
-    
-    // INFORMACIÓN GENERAL
+     // INFORMACIÓN GENERAL
     char frecString[10]; 
     dtostrf(LMIC.freq/1000000.0,3,2,frecString);
     char paqString[10]; 
@@ -247,31 +206,49 @@ void do_send(osjob_t* j){
     u8g2.clearBuffer();
     u8g2.clearDisplay();
     u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawStr(0, 10, "Sensor Puerta");
+    u8g2.drawStr(0, 10, "Sensor Caida");
     u8g2.drawStr(0, 30, "Frecuencia: ");
     u8g2.drawStr(68,30,frecString);
     u8g2.drawStr(100,30," Mhz");
     u8g2.drawStr(0,50,"Paquete: ");
     u8g2.drawStr(50,50,paqString);
     u8g2.sendBuffer();
-    //delay(2000);
+    //delay(3000);
+
+    // Pantalla estado actual
     
-    // Contador de paquetes enviados a TTN
-    paqCont++;  
+    // Se llama funcion de revision
+    revisarEstado();
+    u8g2.clearBuffer();
+    u8g2.clearDisplay();
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    u8g2.drawStr(40, 15, "Estado");
+
+    char estadoString[2]; 
+    dtostrf(estado,2,0,estadoString);
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    if (estado == 0){
+      u8g2.drawStr(62, 30,estadoString);
+      u8g2.drawStr(15, 50,"Si hay electricidad");
+    }
+    else {
+      u8g2.drawStr(62, 30,estadoString);
+      u8g2.drawStr(15, 50,"No hay electricidad");
+    }
+    u8g2.sendBuffer(); 
+     
+    paqCont++;
 }
 
 void setup() {
-    pinMode(puerta1.PIN, INPUT_PULLUP);
-    attachInterrupt(puerta1.PIN, isr, FALLING);
     Serial.begin(115200);
     Serial.println(F("Starting"));
     u8g2.begin();
-//    u8g2.setFont(u8x8_font_chroma48medium8_r);
     #ifdef VCC_ENABLE
-    // For Pinoccio Scout boards
+    // For Pinoccio Scout boardsc
     pinMode(VCC_ENABLE, OUTPUT);
     digitalWrite(VCC_ENABLE, HIGH);
-    delay(1000);
+    delay(3000);
     #endif
 
     // LMIC init
@@ -279,8 +256,9 @@ void setup() {
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
 
-    LMIC_selectSubBand(1);
-
+   for (int channel=0; channel<8; ++channel) {
+     LMIC_disableChannel(channel);
+   }
    for (int channel=9; channel<72; ++channel) {
       LMIC_disableChannel(channel);
    }
@@ -288,16 +266,15 @@ void setup() {
     // Disable link check validation
     LMIC_setLinkCheckMode(0);
     // Set data rate and transmit power (note: txpow seems to be ignored by the library)
-//    LMIC_setDrTxpow(DR_SF7,14);
-    LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 100);
-    // código sensor
-    mydata[0] = 0x16;
+    LMIC_setDrTxpow(DR_SF7,14);
 
+    // Tipo sensor
+    mydata[0] = 0x02;
 
     // Start job
     do_send(&sendjob);
 }
 
 void loop() {
-    os_runloop_once();
+  os_runloop_once();
 }
