7fa8c7eec97ef37f66d6c1d45d0c4c633cccfd68
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-02-26 11:04:41 -0600

    Cambios menos en OTAA/Caida y OTAA/Gotas

--- TheThingsNetwork/Conectores/Arduino/OTAA/Gotas/Gotas.ino
+++ TheThingsNetwork/Conectores/Arduino/OTAA/Gotas/Gotas.ino
@@ -147,6 +147,7 @@ void onEvent (ev_t ev) {
             break;
         case EV_JOINED:
             Serial.println(F("EV_JOINED"));
+            LMIC_setLinkCheckMode(0);
             break;
         case EV_RFU1:
             Serial.println(F("EV_RFU1"));
@@ -257,18 +258,21 @@ void setup() {
     os_init();
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
+    LMIC_selectSubBand(1);
 
-   for (int channel=0; channel<8; ++channel) {
-     LMIC_disableChannel(channel);
-   }
-   for (int channel=9; channel<72; ++channel) {
-      LMIC_disableChannel(channel);
-   }
+ //Disable FSB1, channels 0-7
+  for (int i = 0; i < 7; i++) {
+    if (i != 10)
+      LMIC_disableChannel(i);
 
-    // Disable link check validation
-    LMIC_setLinkCheckMode(0);
-    // Set data rate and transmit power (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(DR_SF7,14);
+  }
+  //Disable FSB2-8, channels 16-72
+  for (int i = 16; i < 73; i++) {
+    if (i != 10)
+      LMIC_disableChannel(i);
+
+  }
+  LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 100);
 
     // Tipo sensor
     mydata[0] = 0x04;
