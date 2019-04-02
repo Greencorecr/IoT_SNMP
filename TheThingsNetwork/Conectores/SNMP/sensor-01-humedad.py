5498a866f0c56a18d5638535091d400ec869885a
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-04-02 12:24:56 -0600

    Moviendo sensor-01-humedad en SNMP/ hacia influxdb

--- TheThingsNetwork/Conectores/SNMP/sensor-01-humedad.py
+++ TheThingsNetwork/Conectores/SNMP/sensor-01-humedad.py
@@ -2,12 +2,7 @@
 # Temperatura y Humedad
 
 from datetime import datetime, timedelta
-from tinydb import TinyDB, Query
-dbpath = "/opt/UNA/MQTT/BD/sensor-01-humedad.json"
-db = TinyDB(dbpath)
-Sensores = Query()
-
-search=db.get(Sensores.dev_id == 'sensor-01-temperatura')
+from influxdb import InfluxDBClient
 
 def TTN2time(search):
     """
@@ -16,14 +11,29 @@ def TTN2time(search):
     timeTTN = datetime.strptime((search.get("time")[0:10] + " " + search.get("time")[11:19]), "%Y-%m-%d %H:%M:%S")
     return timeTTN
 
+# SELECT LAST("water_level") FROM "h2o_feet" WHERE "location" = 'santa_monica'
+host='localhost'
+port=8086
+user = 'root'
+password = 'root'
+dbname = 'sensores'
+dbuser = 'greencore'
+dbuser_password = 'my_secret_password'
+query = 'select LAST(Hum) FROM sensortemphum;'
+influxclient = InfluxDBClient(host, port, user, password, dbname)
+influxresult = influxclient.query(query)
+result = list(influxresult.get_points())
+hum = result[0]['last']
+timeTTN = datetime.strptime(result[0]['time'][0:10] + " " + result[0]['time'][11:19], "%Y-%m-%d %H:%M:%S")
+
 # Medimos el tiempo según TTN, el tiempo de hace 5 minutos. Si no hemos recibido datos, el sensor responde "0"
-timeTTN=TTN2time(search)
 timeFail=datetime.utcnow()
 # Debug
+#print(hum, timeTTN)
 #print (timeFail - timedelta(minutes=5), timeTTN)
 
 print('humedad')
 if (timeFail - timedelta(minutes=5) < timeTTN):
-    print(search.get("hum"))
+    print(hum)
 else:
     print("0")
