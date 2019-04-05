73844036db0187955a0ffbd297ec8fd13b4a9108
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-04-05 11:03:21 -0600

    Agregando script para inicializar base de datos influxdb

---
+++ TheThingsNetwork/Conectores/MQTT/influxdb-init.py
@@ -0,0 +1,19 @@
+from influxdb import InfluxDBClient
+
+def main(host='localhost', port=8086):
+    user = 'root'
+    password = 'root'
+    dbname = 'sensores'
+    dbuser = 'greencore'
+    dbuser_password = 'my_secret_password'
+
+    client = InfluxDBClient(host, port, user, password, dbname)
+
+    print("Create database: " + dbname)
+    client.create_database(dbname)
+
+    print("Create a retention policy")
+    client.create_retention_policy('awesome_policy', '3d', 3, default=True)
+
+if __name__ == '__main__':
+    main()
